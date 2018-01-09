5c640092652cbb65c266e4443810f18d4f3400a3
nirun <ngamkerd@gmail.com>
2018-01-09 15:44:18 +0700

    back control

--- app/src/main/java/nirun/krirk/ac/th/niruncalculate/fragment/CalculateFragment.java
+++ app/src/main/java/nirun/krirk/ac/th/niruncalculate/fragment/CalculateFragment.java
@@ -4,10 +4,13 @@ package nirun.krirk.ac.th.niruncalculate.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import nirun.krirk.ac.th.niruncalculate.MainActivity;
 import nirun.krirk.ac.th.niruncalculate.R;
 
 
@@ -18,6 +21,32 @@ import nirun.krirk.ac.th.niruncalculate.R;
 
 public class CalculateFragment extends Fragment{
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Create Toolbar
+        createToolbar();
+
+
+    }// Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar()
+                .setTitle("Calculate");
+        ((MainActivity)getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
 
     @Nullable
     @Override
